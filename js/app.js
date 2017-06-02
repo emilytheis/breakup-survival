d07199a10b6adad1dcf3d7149ccfb9f5174eb0b2
Katie Broida <katie.broida@upstatement.com>
2017-06-02 16:57:25 -0400

    Prints content on the page ... eventually

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 function init() {
   Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1ZqCUv_Ps0lHS0_I8Onk_xcdP9ThUS2ALtmxre5o7h5Q/pub?output=csv',
-                   callback: function(data, tabletop) { 
-                       console.log(data)
+                   callback: function(data, tabletop) {
+                      var dbRow = Math.random() * (data.length - 1) + 1;
+                      dbRow = Math.round(dbRow);
+                      var suggestionData = data[dbRow];
+                      var elements = ['type', 'name', 'suggestion', 'comment'];
+
+                      // Grab the content and put 'er in
+                      elements.forEach( function (el, index, elements) {
+                        elements[index] = document.getElementById(el);
+                        elements[index].textContent = suggestionData[el];
+                      });
                    },
                    simpleSheet: true } )
 }
