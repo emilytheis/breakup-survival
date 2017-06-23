1cf8c543e3dc61b56c159bd1f118c72cbeb9e523
Regis Biron <regis.biron@upstatement.com>
2017-06-23 16:39:14 -0400

    added some animations, updated readme, emily add in your css for the beating heart loader

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # breakup-survival
+
+## To start a local Python server 
+`python -m SimpleHTTPServer 3000`
+
+## If you have php globally 
+`php -S localhost:3000`
+
--- js/app.js
+++ js/app.js
@@ -1,17 +1,48 @@
-function init() {
-  Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1ZqCUv_Ps0lHS0_I8Onk_xcdP9ThUS2ALtmxre5o7h5Q/pub?output=csv',
-                   callback: function(data, tabletop) {
-                      var dbRow = Math.random() * (data.length - 1) + 1;
-                      dbRow = Math.round(dbRow);
-                      var suggestionData = data[dbRow];
-                      var elements = ['type', 'name', 'suggestion', 'comment'];
+$(document).ready(function($) {
+  var tabletopData;
 
-                      // Grab the content and put 'er in
-                      elements.forEach( function (el, index, elements) {
-                        elements[index] = document.getElementById(el);
-                        elements[index].textContent = suggestionData[el];
-                      });
-                   },
-                   simpleSheet: true } )
-}
-window.addEventListener('DOMContentLoaded', init)
+  // This is a click event that allows us to trigger the randomize data funtion
+  $('.js--next-suggestion').on('click', function(event) {
+    event.preventDefault();
+
+    $('.animate-content').transition({
+      opacity: 0
+    }, 500, 'easeOutExpo', 
+    function() {
+      randomizeData(tabletopData);
+      $('.animate-content').transition({ opacity: 1 });
+    }); 
+  });
+
+  // This initially gets the data from the spreadheet and transforms it into an object
+  function getCardData() {
+    Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1ZqCUv_Ps0lHS0_I8Onk_xcdP9ThUS2ALtmxre5o7h5Q/pub?output=csv',
+    
+    callback: function(data, tabletop) {
+      tabletopData = data;
+      randomizeData(tabletopData);
+
+      if($('.beating-hearts-baby').length) {
+        $('body').removeClass('beating-hearts-baby');
+      }
+    },
+    simpleSheet: true } );
+  }
+
+  // Randomize 
+  function randomizeData(data) {
+    var dbRow = Math.random() * (data.length - 1) + 1;
+    
+    dbRow = Math.round(dbRow);
+    var suggestionData = data[dbRow];
+    var elements = ['type', 'name', 'suggestion', 'comment'];
+
+    // Grab the content and put 'er in
+    elements.forEach( function (el, index, elements) {
+      elements[index] = document.getElementById(el);
+      elements[index].textContent = suggestionData[el];
+    });
+  }
+
+  getCardData();
+});
