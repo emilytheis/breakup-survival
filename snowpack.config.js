b39434b63a68aece2d121ab4bdc0ee126d6cef0a
julialeague <julia.l.allen@gmail.com>
2022-06-18 17:01:08 -0700

    Update buildOptions in snowpack config

--- snowpack.config.js
+++ snowpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    baseUrl: '/breakup-survival',
+    baseUrl: 'http://breakupsurvival.guide',
     // put the build files in /docs
     out: 'docs',
     // put the meta snowpack build files under snowpack instead of _snowpack since Github special-cases underscore prefixed folders
